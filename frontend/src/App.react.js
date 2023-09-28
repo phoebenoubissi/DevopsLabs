56915e33077f0754118e290a6d36c105299ab6e2
phoebenoubissi <phoebesi20@gmail.com>
2023-09-28 09:18:44 -0400

    update on package.json, app.react and sitewrapper.js

--- frontend/src/App.react.js
+++ frontend/src/App.react.js
@@ -1,6 +1,9 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApmRoute } from '@elastic/apm-rum-react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import { Route } from 'react-router-dom';
+
+
+//import { Route } from '@elastic/apm-rum-react'
 //import { init as initApm } from "@elastic/apm-rum";
 
 import HomePage from "./HomePage.react";
@@ -10,7 +13,7 @@ import AttendanceForm from "./AttendanceForm";
 import ListAttendance from './AttendanceList';
 import ListSalary from './ListSalary';
 
-import "tabler-react/dist/Tabler.css";
+//import "tabler-react/dist/Tabler.css";
 
 type Props = {||};
 
@@ -24,17 +27,18 @@ function App(props: Props): React.Node {
   return (
     <React.Fragment>
       <Router>
-        <Switch>
-          <ApmRoute exact path="/" component={HomePage} />
-          <ApmRoute exact path="/employee-add" component={EmployeeForm} />
-          <ApmRoute exact path="/employee-list" component={EmployeeList} />
-          <ApmRoute exact path="/attendance-add" component={AttendanceForm} />
-          <ApmRoute exact path="/attendance-list" component={ListAttendance} />
-          <ApmRoute exact path="/salary-list" component={ListSalary} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/employee-add" component={EmployeeForm} />
+          <Route exact path="/employee-list" component={EmployeeList} />
+          <Route exact path="/attendance-add" component={AttendanceForm} />
+          <Route exact path="/attendance-list" component={ListAttendance} />
+          <Route exact path="/salary-list" component={ListSalary} />
+        </Routes>
       </Router>
     </React.Fragment>
   );
 }
 
 export default App;
+
